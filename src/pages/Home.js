56cd937cba72c5763de32fc23a6a2b4d69bcb60f
Joe <joesamere00@gmail.com>
2022-05-18 09:41:48 -0600

    Initial Push

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainPage() {
+  return <div>
+    <div className="container">
+        <a href="/dashboard"><span>Enter</span></a>
+    </div>
+  </div>;
+}
+
+export default MainPage;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './MainPage.css';
+
+function MainPage() {
+  return <div>
+    <h1 className='primary'>THIS IS THE Main Page!</h1>
+  </div>;
+}
+
+export default MainPage;
